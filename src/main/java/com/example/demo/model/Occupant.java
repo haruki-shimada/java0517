3c422bc00fc7544779c4bc8f5507822ef910a2e3
imnota <>
2024-05-17 15:11:03 +0900

    入居者情報のフィールドおよびgetter, setterの作成

--- src/main/java/com/example/demo/model/Occupant.java
+++ src/main/java/com/example/demo/model/Occupant.java
@@ -1,5 +1,86 @@
 package com.example.demo.model;
 
-public class Occupant {
+import java.sql.Date;
 
+import com.example.demo.data.ApartmentInterface;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "apartment")
+public class Occupant implements ApartmentInterface {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column
+    private String salesrep;
+
+    @Column
+    private float floorarea;
+
+    @Column
+    private Date registerdate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getSalesrep() {
+		return salesrep;
+	}
+
+	public void setSalesrep(String salesrep) {
+		this.salesrep = salesrep;
+	}
+
+	public float getFloorarea() {
+		return floorarea;
+	}
+
+	public void setFloorarea(float floorarea) {
+		this.floorarea = floorarea;
+	}
+
+	public Date getRegisterdate() {
+		return registerdate;
+	}
+
+	public void setRegisterdate(Date registerdate) {
+		this.registerdate = registerdate;
+	}
+    
+    
 }
