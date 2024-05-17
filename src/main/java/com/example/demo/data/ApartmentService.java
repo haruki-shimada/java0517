b7ceb98029453a388bed987a14772a847403305f
imnota <>
2024-05-17 15:22:33 +0900

    serviceの作成

--- src/main/java/com/example/demo/data/ApartmentService.java
+++ src/main/java/com/example/demo/data/ApartmentService.java
@@ -1,5 +1,22 @@
 package com.example.demo.data;
 
-public class ApartmentService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.repositories.ApartmentRepository;
+
+public class ApartmentService {
+    @Autowired
+    private ApartmentRepository repository;
+
+    public List<? extends ApartmentInterface> getAll() {
+        System.out.println("Service:getAll");
+        return repository.findAll();
+    }
+
+    public ApartmentInterface getById(int id) {
+        System.out.println("Service:getById[" + id + "]");
+        return repository.findById.orElse(null);
+    }
 }
