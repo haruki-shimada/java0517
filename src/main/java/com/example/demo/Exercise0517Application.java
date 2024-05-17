03078fa88ec83233eab0f12bb1cc3ee47fad5b95
imnota <>
2024-05-17 13:57:09 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/Exercise0517Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise0517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise0517Application.class, args);
+	}
+
+}
