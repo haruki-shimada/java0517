a5af73e85632bc8578ac2bb2d4f1a77ddeb73e29
imnota <>
2024-05-17 15:28:30 +0900

    リポジトリの作成

--- src/main/java/com/example/demo/repositories/ApartmentRepository.java
+++ src/main/java/com/example/demo/repositories/ApartmentRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo.repositories;
 
-public class ApartmentRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Occupant;
+
+@Repository
+public interface ApartmentRepository extends JpaRepository<Occupant, Integer> {
+    public Optional<Occupant> findById(int id);
 }
