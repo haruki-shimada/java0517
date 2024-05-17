77de771d10c292744dca89db1b87554d82016cee
imnota <>
2024-05-17 15:13:29 +0900

    入居者情報のインターフェイス作成

--- src/main/java/com/example/demo/data/ApartmentInterface.java
+++ src/main/java/com/example/demo/data/ApartmentInterface.java
@@ -1,5 +1,29 @@
 package com.example.demo.data;
 
-public interface ApartmentInterface {
+import java.sql.Date;
 
+public interface ApartmentInterface {
+    public int getId();
+
+    public void setId(int id);
+
+    public String getName();
+
+    public void setName(String name);
+
+    public String getAddress();
+
+    public void setAddress(String address);
+
+    public String getSalesrep();
+
+    public void setSalesrep(String salesrep);
+
+    public float getFloorarea();
+
+    public void setFloorarea(float floorarea);
+
+    public Date getRegisterdate();
+
+    public void setRegisterdate(Date registerdate);
 }
